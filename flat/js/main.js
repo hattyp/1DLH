82bb764955fcdde9ea1e9e0c53ebcc7c5f6953db
Harriet <harriet@littlereddiesel.com>
2018-03-22 15:33:50 +0000

    ZAYN update, directory moved

---
+++ flat/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+	$('.partner.status.louis').mouseover(function(){
+	 	$('#louisPic').attr('src', 'img/eleanor-calder.jpg');
+	}).mouseout(function(){
+	  	$('#louisPic').attr('src', 'img/louis-new.jpg');
+	});
+
+	// $('.partner.status.zayn').mouseover(function(){
+	//  	$('#zaynPic').attr('src', 'img/gigi-hadid.jpg');
+	// }).mouseout(function(){
+	//   	$('#zaynPic').attr('src', 'img/zayn-new.jpg');
+	// });
+
+	$('.partner.status.liam').mouseover(function(){
+	 	$('#liamPic').attr('src', 'img/cheryl.jpg');
+	}).mouseout(function(){
+	  	$('#liamPic').attr('src', 'img/liam-new.jpg');
+	});
+
+});
+
+
